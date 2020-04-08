1a1309f7ce9ec766f68e009d90362213c03df6f5
fee <felipetadra@gmail.com>
2020-04-08 13:20:20 -0300

    fix: bugs

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizModalComponent } from '../../modals/quiz/quiz.component';
 import { ModalService } from '../../../../services/modal.service';
-import { FollowUpModalComponent } from 'src/app/pages/flow/modals/follow-up/follow-up.component';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -17,6 +16,6 @@ export class HomeHeroComponent implements OnInit {
   }
 
   openQuiz() {
-    this.modalService.open(FollowUpModalComponent);
+    this.modalService.open(QuizModalComponent);
   }
 }
